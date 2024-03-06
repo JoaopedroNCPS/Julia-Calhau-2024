e3d89c24a88b8bf8b9edc5ae03b9bb91251f3a7c
JoaopedroNCPS <160642137+JoaopedroNCPS@users.noreply.github.com>
2024-03-06 15:58:04 -0300

    Update somaa.c

--- somaa.c
+++ somaa.c
@@ -5,12 +5,12 @@
 int main()
 {
     setlocale(LC_ALL,"");
-    long long int A, B, C;
+    long long int A, B;
     printf("Digite DOIS numeros inteiros:");
-    scanf("%lld %lld %lld", &A, &B, &C);
+    scanf("%lld %lld", &A, &B);
     printf("O numero %lld multiplicado por 4 Bi é %lld", A, A * 200000000);
     printf("\n");
     printf("O numero %lld multiplicado por 4 Bi é %lld", B, B * 200000000);
     return 0;
 }
-         
+            
