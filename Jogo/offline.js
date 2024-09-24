5b978886f00aedc70f19a9b7ecd2c03f2f4026cc
João Pedro N. <160642137+JoaopedroNCPS@users.noreply.github.com>
2024-09-24 10:36:03 -0300

    Add files via upload

---
+++ Jogo/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1724264778,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/per-sheet0.png",
+		"images/per-sheet1.png",
+		"images/morango-sheet0.png",
+		"images/morango-sheet1.png",
+		"images/morango-sheet2.png",
+		"images/sprite-sheet0.png",
+		"images/s-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
---
+++ Projeto-Olimpiadas/script.js
@@ -0,0 +1,10 @@
+bronze = document.querySelector('.bro')
+prata = document.querySelector('.pra')
+ouroo = document.querySelector('.ouro')
+tyu = document.querySelector('.ba')
+
+resultado = bronze + prata + ouroo;
+
+function gugu() {
+    tyu.innerHTML = `Total de Medalhas: ${resultado}`;
+} 
